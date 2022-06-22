bea79b0eb39bbbad7ba75b047942234478451763
olddevpat <patdevwork.40s@gmail.com>
2022-06-22 12:51:09 +0800

    first commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(
+            "mongodb+srv://project1:<9182736>@cluster0.ztmk9lv.mongodb.net/todolist?retryWrites=true&w=majority",
+            connectionParams
+        );
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
